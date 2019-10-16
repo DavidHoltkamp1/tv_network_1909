96d83fa8eef9e701c7a74b74f506e311cd5f278b
David Holtkamp <david.hholtkamp@gmail.com>
2019-10-16 10:52:00 -0600

    Create class and initialize, add show

---
+++ lib/network.rb
@@ -0,0 +1,20 @@
+class Network
+attr_reader :name
+attr_accessor :shows
+
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def highest_paid_actor
+    @shows.map  do |show|
+      show.total_salary
+    end
+
+  end
+end
